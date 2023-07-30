a8e4f1fa7b0fd5154bab82c4be6beef4c1e4d4a6
Erick <esdn66@hotmail.com>
2023-07-30 08:36:57 -0300

    add verify position

--- arvore/graph.py
+++ arvore/graph.py
@@ -1,57 +1,76 @@
-import matplotlib.pyplot as plt
-from IPython.display import display
-import pandas as pd
-import networkx as nx
-import os
 import itertools
+import os
 
-from utils import load_graph
-from utils import return_components_in_topological_order
+import matplotlib.pyplot as plt
+import networkx as nx
+import pandas as pd
+from IPython.display import display
+from utils import load_graph, return_components_in_topological_order, Graph
 
 node_path = os.path.join(os.path.dirname(__file__), "bio-t.csv")
 edge_path = os.path.join(os.path.dirname(__file__), "bio-relate.csv")
 
 graph, labels, label_mapping = load_graph(node_path, edge_path)
 
-#nx.draw(graph, labels=labels, with_labels=True)
-#plt.show()
-nx.draw(graph, labels=label_mapping, with_labels=True)
+# nx.draw(graph, labels=labels, with_labels=True)
+# plt.show()
+#nx.draw(graph, labels=label_mapping, with_labels=True)
 plt.show()
 
 order = return_components_in_topological_order(graph)
 flattened_list = list(itertools.chain.from_iterable(order))
-df = pd.read_csv(node_path, index_col='id')
+df = pd.read_csv(node_path, index_col="id")
 df_edges = pd.read_csv(edge_path)
 df_edges = df_edges.dropna().reset_index(drop=True)
 
-i=1
+i = 1
 for subgraph in order:
     for node in subgraph:
-
         x1 = i
-        y1 = -df.loc[node, 'inicio']
+        y1 = -df.loc[node, "inicio"]
 
         x2 = i
-        y2 = -df.loc[node, 'fim']
+        y2 = -df.loc[node, "fim"]
 
-        plt.plot([x1, x2], [y1, y2], color='orange', linestyle='-', linewidth=6)
-        plt.annotate(df.loc[node, 'nome'], (x1, y1), color='black', fontsize=12, ha='center', va='center')
+        plt.plot([x1, x2], [y1, y2], color="orange", linestyle="-", linewidth=6)
+        plt.annotate(
+            df.loc[node, "nome"],
+            (x1, y1),
+            color="black",
+            fontsize=12,
+            ha="center",
+            va="center",
+        )
 
-        #take the lines that have the node as origin
-        df_edges_origin = df_edges[df_edges['O-ID'] == node]
+        # take the lines that have the node as origin
+        df_edges_origin = df_edges[df_edges["O-ID"] == node]
         for index, row in df_edges_origin.iterrows():
             x1 = i
-            y1 = -df.loc[row['D-ID'], 'inicio']
+            y1 = -df.loc[row["D-ID"], "inicio"]
 
-            x2 = flattened_list.index(row['D-ID']) + 1
+            x2 = flattened_list.index(row["D-ID"]) + 1
             y2 = y1
 
-            plt.arrow(x1, y1, x2-x1, y2-y1, color='black', linestyle='-', linewidth=1, head_width=0.1, head_length=0.1, length_includes_head=True)
-            plt.scatter(x1, y1, color='green', s=50)
-            plt.scatter(x2, y2, color='black', s=100)
-
+            plt.arrow(
+                x1,
+                y1,
+                x2 - x1,
+                y2 - y1,
+                color="black",
+                linestyle="-",
+                linewidth=1,
+                head_width=0.1,
+                head_length=0.1,
+                length_includes_head=True,
+            )
+            plt.scatter(x1, y1, color="green", s=50)
+            plt.scatter(x2, y2, color="black", s=100)
 
         i += 1
-
 plt.show()
+
+grafo = Graph(label_mapping=label_mapping, components_in_topological_order=order, df_nodes=df, df_edges=df_edges)
+grafo.draw()
+#grafo.print_info()
+
 
--- arvore/utils.py
+++ arvore/utils.py
@@ -1,5 +1,9 @@
+import itertools
+
+import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
+
 
 def create_graph(nodes, edges, label):
     lab = {}
@@ -12,9 +16,10 @@ def create_graph(nodes, edges, label):
     for i in range(len(edges)):
         G.add_edge(edges[i][0], edges[i][1])
 
-    label_mapping = {node: data['label'] for node, data in G.nodes(data=True)}
+    label_mapping = {node: data["label"] for node, data in G.nodes(data=True)}
 
     return G, lab, label_mapping
+
 
 def load_graph(path_nodes, path_edges):
     df = pd.read_csv(path_nodes)
@@ -34,6 +39,7 @@ def load_graph(path_nodes, path_edges):
 
     return create_graph(nodes, edges, label)
 
+
 def return_components_in_topological_order(graph):
     components = list(nx.weakly_connected_components(graph))
     components_in_topological_order = []
@@ -42,3 +48,149 @@ def return_components_in_topological_order(graph):
         topological_order = list(nx.topological_sort(subgraph))
         components_in_topological_order.append(topological_order)
     return components_in_topological_order
+
+
+class Node:
+    def __init__(self, id, label, inicio, fim):
+        self.id = id
+        self.nome = label
+        self.begin = inicio
+        self.end = fim
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
+
+    def set_x1(self, x1):
+        self.x1 = x1
+
+    def set_x2(self, x2):
+        self.x2 = x2
+
+    def set_y1(self, y1):
+        self.y1 = y1
+
+    def set_y2(self, y2):
+        self.y2 = y2
+
+
+class Edge:
+    def __init__(self, origin_id, destination_id):
+        self.origin_id = origin_id
+        self.destination_id = destination_id
+        self.origin_x = 0
+        self.origin_y = 0
+        self.destination_x = 0
+        self.destination_y = 0
+
+    def set_origin_x(self, origin_x):
+        self.origin_x = origin_x
+
+    def set_origin_y(self, origin_y):
+        self.origin_y = origin_y
+
+    def set_destination_x(self, destination_x):
+        self.destination_x = destination_x
+
+    def set_destination_y(self, destination_y):
+        self.destination_y = destination_y
+
+
+class Graph:
+    def __init__(
+        self, label_mapping, components_in_topological_order, df_nodes, df_edges
+    ):
+        self.label_mapping = label_mapping
+        self.components_in_topological_order = components_in_topological_order
+        self.nodes_list = {}
+        self.edges_list = []
+        self.df_edges = df_edges
+        self.df_nodes = df_nodes
+
+    def set_nodes_coordinates(self):
+        i = 1
+        for subgraph in self.components_in_topological_order:
+            for node in subgraph:
+                new_node = Node(
+                    id=node,
+                    label=self.df_nodes.loc[node, "nome"],
+                    inicio=self.df_nodes.loc[node, "inicio"],
+                    fim=self.df_nodes.loc[node, "fim"],
+                )
+                new_node.set_x1(i)
+                new_node.set_y1(-self.df_nodes.loc[node, "inicio"])
+
+                new_node.set_x2(i)
+                new_node.set_y2(-self.df_nodes.loc[node, "fim"])
+
+                self.nodes_list[node] = new_node
+                i += 1
+
+    def set_edges_coordinates(self):
+        for index, row in self.df_edges.iterrows():
+            new_edge = Edge(origin_id=row["O-ID"], destination_id=row["D-ID"])
+
+            y1 = self.nodes_list[row["D-ID"]].y1
+
+            new_edge.set_origin_x(self.nodes_list[row["O-ID"]].x1)
+            new_edge.set_origin_y(y1)
+
+            new_edge.set_destination_x(self.nodes_list[row["D-ID"]].x1)
+            new_edge.set_destination_y(y1)
+            self.edges_list.append(new_edge)
+
+    def draw(self):
+        self.set_nodes_coordinates()
+        self.set_edges_coordinates()
+
+        for node in self.nodes_list.values():
+            plt.plot(
+                [node.x1, node.x2],
+                [node.y1, node.y2],
+                color="orange",
+                linestyle="-",
+                linewidth=6,
+            )
+            plt.annotate(
+                node.nome,
+                (node.x1, node.y1),
+                color="black",
+                fontsize=12,
+                ha="center",
+                va="center",
+            )
+
+        for edge in self.edges_list:
+            plt.arrow(
+                edge.origin_x,
+                edge.origin_y,
+                edge.destination_x - edge.origin_x,
+                edge.destination_y - edge.origin_y,
+                color="black",
+                length_includes_head=True,
+                head_width=0.1,
+                head_length=0.1,
+                width=0.001,
+            )
+
+        plt.show()
+
+        def print_info(self):
+            print("Nodes:")
+            for node in self.nodes_list.values():
+                print(node.id, node.label, node.begin, node.end)
+            print("Edges:")
+            for edge in self.edges_list:
+                print(edge.origin_id, edge.destination_id)
+        
+        def empty_space(self, x, y):
+            '''Verify if there is an node in the given coordinates'''
+            for node in self.nodes_list.values():
+                if not node.x1 == node.x2:
+                    raise Exception("Node is not vertical")
+                
+                if node.x1 == x and node.x2 == x:
+                    if node.y1 <= y <= node.y2 or node.y2 <= y <= node.y1:
+                        return False
+
+            return True
